8e84d2334535b3d6d75d8932a0c6b4758f5097b0
codeaj001 <codeaj001@gmail.com>
2025-05-10 10:52:20 +0000

    bn.js cleared

--- vite.config.ts
+++ vite.config.ts
@@ -22,6 +22,7 @@ export default defineConfig({
       "@": path.resolve(import.meta.dirname, "client", "src"),
       "@shared": path.resolve(import.meta.dirname, "shared"),
       "@assets": path.resolve(import.meta.dirname, "attached_assets"),
+ 'bn.js': 'bn.js/lib/bn.js',
     },
   },
   root: path.resolve(import.meta.dirname, "client"),
